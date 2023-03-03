f2f053556bac68eaa0311a8a6172c8ce17bac2c2
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-03-03 17:33:16 +0530

    new api

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ async function fetctUserweatherdata(coordinate){
         grantlocation_tab.classList.remove('active');
         loader.classList.add('active');
         const response =
-        await fetch(`https://api.openweathermap.org/data/2.5/weather?               
-        lat=${lat}&lon=${lon}&appid=6e465ced7f21c5207b02e1ebc68abaf0`);
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=26.811712&lon=79.004692&appid=6e465ced7f21c5207b02e1ebc68abaf0`);
         const data = await response.json();
         loader.classList.remove('active');
         renderUI(data);
